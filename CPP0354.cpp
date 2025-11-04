6d2b71eadfec05cc6b582c86c1740b61e7440167
Nguyễn Đức Hiếu <hieunguyenduc2021@gmail.com>
2025-11-04 09:17:59 +0700

    Update CPP0354.cpp

--- CPP0354.cpp
+++ CPP0354.cpp
@@ -1,25 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 #define ll long long 
-using namespace std ;
- 
-  
-  int main(){
-  	ll t;
-  	cin >> t ;
-  	cin.ignore();
-  	while(t--){
-  		string s ;
-  		getline(cin,s);
-  		ll count = 1;
-  		for(ll i = 1 ; i <= s.size(); i++){
-  			if(s[i]==s[ i - 1]){
-  				count++;
-  				}
-  				else{
-  				cout << s[i - 1] << count ;
-				  count = 1;	
-				  }
-  		}
-  		cout << endl;
-  		}
-  		}
+
+int main (){
+    ll t;
+    cin >> t;
+    cin.ignore();
+    while(t--){
+        string s;
+        getline(cin, s);
+        map<char, ll> freq;
+        for (auto c : s) {
+            freq[c]++;
+        }
+
+        set<char> printed;
+        for (auto c : s) {
+            if (printed.count(c) == 0) {//kiểm tra xem kí tự c chưa in lần nào.
+                cout << c << freq[c];
+                printed.insert(c);
+            }
+        }
+
+        cout << endl;
+    }
+}
